210be6d53131177fa82a6a999ee8a2ad91b64aab
Kimberly Kohel-Hayes <kohelka@gmail.com>
2020-05-20 13:27:09 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
 document.body.appendChild(element);
+element.style.textAlign = 'center';
+
+
+
